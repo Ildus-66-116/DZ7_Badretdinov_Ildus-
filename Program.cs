cb07f81e28a72c5388256dedb6127cb22eca6a8c
ILDUS <ildus6696@gmail.com>
2023-06-13 21:05:22 +0300

    Reshena Zadacha 50

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@
 
 // void InputMatrix(double[,] matrix)
 // {
-//     for(int i = 0; i < matrix.GetLength(0); i++) // GetLength(0) количество строк
+//     for(int i = 0; i < matrix.GetLength(0); i++)
 //     {
-//         for(int j = 0; j < matrix.GetLength(1); j++) // GetLength(1) количестов столбцов
+//         for(int j = 0; j < matrix.GetLength(1); j++)
 //             matrix[i, j] = Math.Round(new Random().NextDouble() * (50 - 100) + 10, 1);
 //     }
 // }
@@ -18,7 +18,7 @@
 //     for(int i = 0; i < matrix.GetLength(0); i++)
 //     {
 //         for(int j = 0; j < matrix.GetLength(1); j++)
-//             Console.Write($"{matrix[i, j]}\t");         // \t табуляция
+//             Console.Write($"{matrix[i, j]}\t");
 //         Console.WriteLine();
 //     }
 // }
@@ -29,3 +29,39 @@
 // double[,] matrix = new double[size[0], size[1]];
 // InputMatrix(matrix);
 // PrintMatrix(matrix);
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+void InputMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 11);
+    }
+}
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]}\t");
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+Console.Write("Введите позицию: ");
+int[] sizeNew = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+Console.Write($"Значение: {matrix[sizeNew[0], sizeNew[1]]}");
